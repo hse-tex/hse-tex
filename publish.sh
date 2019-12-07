b8c627a10f62e1be5e67802bd2bcda9053e2b6d6
darkkeks <darkkeks@rambler.ru>
2019-12-07 15:14:13 +0300

    Travis setup

--- publish.sh
+++ publish.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
+set -e
 
-files=$(./create_file_list.sh)
-
-branch=$(git branch --show-current)
-git checkout gh-pages
-
-# relying on .pdf not being touched by git)00)00)0
-cp -r .pdf/* .
-
-# copy README from master
+# branch=$(git rev-parse --abbrev-ref HEAD)
+# 
+# git fetch origin gh-pages:gh-pages
+# git checkout gh-pages
+# 
+# # relying on .pdf not being touched by git)00)00)0
+# cp -r .pdf/* .
+# 
+# # copy README from master
 # git checkout master README.md
+# 
+# git add -A
+# git commit -m "${TRAVIS_COMMIT_MESSAGE:=pdf updates}"
+# git push origin gh-pages
+# 
+# git checkout $branch
+
+
+git clone --branch gh-pages git@github.com:LoDThe/hse-tex.git _gh_pages
+pushd _gh_pages
+
+cp -r ../.pdf/* .
+cp ../README.md .
 
 git add -A
-git commit --allow-empty -m "${TRAVIS_COMMIT_MESSAGE:=pdf updates}"
+git commit -m "${TRAVIS_COMMIT_MESSAGE:=pdf updates}"
 git push origin gh-pages
 
-git checkout $branch
+popd
