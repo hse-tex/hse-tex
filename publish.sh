8808161d138192a0963598b87c0050dbeca2344e
darkkeks <darkkeks@rambler.ru>
2020-08-30 12:27:33 +0300

    Use github actions

--- publish.sh
+++ publish.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
 set -e
 
-git clone --branch gh-pages git@github.com:LoDThe/hse-tex.git _gh_pages
+function get_commit_message {
+    if [[ -n "$TRAVIS_COMMIT_MESSAGE" ]]; then
+        echo $TRAVIS_COMMIT_MESSAGE
+    elif [[ -n "$GITHUB_SHA" ]]; then
+        echo $(git log --format=%B -n 1 $GITHUB_SHA)
+    fi
+}
+
+if [[ ! -d _gh_pages ]]; then
+    git clone --branch gh-pages git@github.com:hse-tex/hse-tex.git _gh_pages
+fi
+
+commit_message=$(get_commit_message)
+git rev-parse HEAD > _gh_pages/.git-revision
+
 pushd _gh_pages
 
 cp -r ../.pdf/* .
 cp ../README.md .
 
 git add -A
-git commit -m "${TRAVIS_COMMIT_MESSAGE:=pdf updates}"
-git push origin gh-pages
+
+if git diff --quiet; then
+    author=${GITHUB_ACTOR:=github-actions}
+    git config user.name "$author"
+    git config user.email "$author@users.noreply.github.com"
+
+    git commit --allow-empty -m "${prev_message:=pdf updates}"
+    git push origin gh-pages
+fi
 
 popd
