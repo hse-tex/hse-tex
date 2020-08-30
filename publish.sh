d89d3a771ba1792261bdd1c5c03e29fb9ee3fea0
DarkKeks <darkkeks@rambler.ru>
2020-08-30 20:56:52 +0300

    Использовать github actions вместо travis-ci (#58)

--- publish.sh
+++ publish.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 set -e
 
-git clone --branch gh-pages git@github.com:LoDThe/hse-tex.git _gh_pages
+function get_commit_message {
+    if [[ -n "$GITHUB_SHA" ]]; then
+        echo $(git log --format=%B -n 1 $GITHUB_SHA)
+    fi
+    echo "pdf updates"
+}
+
+if [[ ! -d _gh_pages ]]; then
+    git clone --branch gh-pages git@github.com:hse-tex/hse-tex.git _gh_pages
+fi
+
+commit_message=$(get_commit_message)
+git rev-parse HEAD > _gh_pages/.git-revision
+
 pushd _gh_pages
 
 cp -r ../.pdf/* .
 cp ../README.md .
 
 git add -A
-git commit -m "${TRAVIS_COMMIT_MESSAGE:=pdf updates}"
-git push origin gh-pages
+
+if git diff --quiet; then
+    author=${GITHUB_ACTOR:=github-actions}
+    git config user.name "$author"
+    git config user.email "$author@users.noreply.github.com"
+
+    git commit --allow-empty -m "$commit_message"
+    git push origin gh-pages
+fi
 
 popd
