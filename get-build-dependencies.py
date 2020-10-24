5ac846143fd5ff1485b637d84ac1a9bfa36654cb
darkkeks <darkkeks@rambler.ru>
2020-10-24 22:45:34 +0300

    Поправить вычисление зависимостей

--- get-build-dependencies.py
+++ get-build-dependencies.py
@@ -15,18 +15,25 @@ def main(args):
     if not source.is_file():
         err("Not a file:", source)
         exit(-1)
-    
+
     result = []
 
     current = Path('.')
     for part in source.parts:
-        if args.single and current == source.parent:
+        if current == source.parent:
+            if args.directory:
+                for file in current.glob('**/*'):
+                    result.append(file)
+            if args.single:
+                for file in current.glob('*header*'):
+                    result.append(file)
             break
 
         for header in current.glob('*.tex'):
             result.append(header)
         for header in current.glob('*.sty'):
             result.append(header)
+
         current /= part
 
     if args.single:
@@ -37,7 +44,7 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Get .tex build dependencies')
-    
+
     mode = parser.add_mutually_exclusive_group(required=True)
     mode.add_argument('--single', '-s', action='store_true', default=False)
     mode.add_argument('--directory', '-d', action='store_true', default=False)
